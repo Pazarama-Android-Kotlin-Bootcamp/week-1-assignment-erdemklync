a365cf4f98b90965d34d86f5d06d02532fc1b131
Erdem Kalyoncu <ekalyoncu81@gmail.com>
2022-09-23 21:16:40 +0300

    Assignments submitted

--- Odev2.kt
+++ Odev2.kt
@@ -7,12 +7,12 @@ private fun main(){
     }while (input.isBlank())
 
     // Gelen input'u bölüp her öğesini integer'a dönüştür ve bunları bir IntArray'e dönüştür.
-    val intArray = input.split(" ").map { it.toInt() }.toIntArray()
+    val intArray = input.split(" ").map { it.toLong() }.toLongArray()
 
     print(aVeryBigSum(intArray))
 }
 
-fun aVeryBigSum(intArray: IntArray): Long {
+fun aVeryBigSum(intArray: LongArray): Long {
     // fold metodu her öğenin üzerinden geçip öğeyi bir toplamda tutmamıza yarıyor.
     return intArray.fold(0){ sum, value -> sum + value }
 }
